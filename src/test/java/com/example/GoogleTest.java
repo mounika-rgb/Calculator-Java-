4a65aa1cd624dc4cade0d48dd73d9d42bfeb813e
Ramesh <ramesh.maltech@gmail.com>
2025-08-01 12:56:08 +0530

    chnaged Test file

--- src/test/java/com/example/GoogleTest.java
+++ src/test/java/com/example/GoogleTest.java
@@ -1,22 +1,23 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+package com.example;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.*;
 
 public class GoogleTest {
-
     WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup(); // Automatically downloads & sets path
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
     @Test
     public void openGoogle() {
         driver.get("https://www.google.com");
-        System.out.println("Title: " + driver.getTitle());
+        System.out.println("Page Title: " + driver.getTitle());
     }
 
     @AfterMethod
